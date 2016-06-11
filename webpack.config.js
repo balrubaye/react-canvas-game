d2366519d67771d154e8666a15d3e6d73229e161
Bassam Al Rubaye <bassam@Bassams-MacBook-Pro.local>
2016-06-10 17:32:24 -0700

    Basic webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path= require('path');
+
+var SRC_DIR= path.resolve( __dirname ,'app/src/');
+var BUILD_DIR= path.resolve( __dirname , 'app');
+
+var config={
+	entry: [SRC_DIR + '/components/index.jsx'],
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	}
+}
+
+module.exports = config;
